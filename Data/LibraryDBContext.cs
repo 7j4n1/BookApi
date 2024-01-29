9a670b25bae40918ff220f9ea80f405494eb63c5
7j4n1 <ahmaholy@gmail.com>
2024-01-29 09:18:19 +0100

    Add LibraryDBContext class to handle database operations

---
+++ Data/LibraryDBContext.cs
@@ -0,0 +1,13 @@
+﻿using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApi;
+
+public class LibraryDBContext(DbContextOptions<LibraryDBContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books { get; set; }
+
+    public DbSet<Author> Authors { get; set; }
+
+    public DbSet<Publisher> Publishers { get; set; }
+}
--- obj/Debug/net8.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e536907130a524816bf9dfa253669aafbf68908d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
