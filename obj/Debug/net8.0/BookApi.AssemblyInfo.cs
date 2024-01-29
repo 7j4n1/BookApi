275c7907d18d3715ecbe91706f7b58dfcc88dae8
7j4n1 <ahmaholy@gmail.com>
2024-01-29 19:20:26 +0100

    dependencies updates

--- obj/Debug/net8.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec1228c8b7b2bcf289c9adbb07f5439e6d844f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830342aad87d7410dce0387e20d7837687286f29")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
