f625606cd67d1624fb13cfe8bcf2ded87e8fc56a
7j4n1 <ahmaholy@gmail.com>
2024-01-29 19:18:23 +0100

    Update project dependencies and configuration

--- Program.cs
+++ Program.cs
@@ -1,16 +1,44 @@
 
+using System.Globalization;
+using BookApi.Cache;
+using BookApi.Services;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace BookApi;
 
-public class Program
+public class Program()
 {
+    public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        builder.Services.AddDbContext<LibraryDBContext>(options => options.UseInMemoryDatabase("LibraryDB"));
+        builder.Services.AddDbContext<LibraryDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddScoped<ICacheService, CacheService>();
+        builder.Services.AddScoped<IAuthorService, AuthorService>();
+
+        builder.Services.Configure<RequestLocalizationOptions>(options =>
+        {
+            var supportedCultures = new[]
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("fr-FR"),
+                // Add more cultures here
+            };
+
+            options.DefaultRequestCulture = new RequestCulture("en-US");
+            options.SupportedCultures = supportedCultures;
+            options.SupportedUICultures = supportedCultures;
+        });
+
+        
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,6 +58,8 @@ public class Program
 
         app.UseAuthorization();
 
+        
+        app.UseRequestLocalization();
 
         app.MapControllers();
 
