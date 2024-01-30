ecf35539e8976e61ba9c9d06d3d3f2d0cec33776
7j4n1 <ahmaholy@gmail.com>
2024-01-30 10:36:02 +0100

    Update BookApi.dll, BookApi.exe, BookApi.pdb, apphost.exe, BookApi.AssemblyInfoInputs.cache, BookApi.sourcelink.json, LibraryDBContextModelSnapshot.cs, and BookApi.AssemblyInfo.cs

---
+++ Migrations/20240130093344_UpdateAllModels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAllModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "PublishedDate",
+                table: "Book",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "PublishedDate",
+                table: "Book",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
