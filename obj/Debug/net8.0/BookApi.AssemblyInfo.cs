20b04ba829f7be927bea01c6b84e7a7c6c0d6ea6
7j4n1 <ahmaholy@gmail.com>
2024-01-30 17:42:10 +0100

    Add CRUD API documentation and installation instructions

--- obj/Debug/net8.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fae3b9181b1cf0b5f8409dab036124687136092")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6e08639b979ab1e84a5672334a8aa05214bc8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
